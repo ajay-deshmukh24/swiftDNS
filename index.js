6f4127548065080ffe1081a93a35126d7c66706e
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-01-09 16:58:08 +0530

    parse the dns query

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import { startUdpServer } from "denamed";
+import { createResponse, createTxtAnswer, startUdpServer } from "denamed";
 
 startUdpServer((query) => {
-  console.log(query);
+  // console.log(query);
+  const question = query.questions[0];
+  console.log("Question :", question);
+
+  return createResponse(query, [createTxtAnswer(question, "hello world")]);
 });
 
-console.log("hii from me");
+// console.log("hii from me");
